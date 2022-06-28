4e495d1563fbac043caa5a990aa12a74c7747570
GageVanK <gagevank@gmail.com>
2022-06-28 19:23:31 -0400

    added React Router and Web Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+import HighScores from "./pages/HighScores";
+import GameOver from "./pages/GameOver";
 
 function App() {
   return (
-    <div>
-      hey
-     </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/game" element={<Game />} />
+        <Route path="/highScores" element={<HighScores />} />
+        <Route path="/gameOver" element={<GameOver />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
