a3d8e38ff25a820043a99f6655556d1840e624a3
GageVanK <gagevank@gmail.com>
2022-06-30 01:05:27 -0400

    base styled-components

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Game from "./pages/Game";
 import HighScores from "./pages/HighScores";
 import GameOver from "./pages/GameOver";
 import NavBar from "./components/NavBar";
+import { Container } from "./styled-components/Container";
+import GlobalStyle from "./styled-components/Global";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
+    
       <NavBar />
+      
+      <Container>
       <Routes>
         <Route path="/game" element={<Game />} />
         <Route path="/highScores" element={<HighScores />} />
         <Route path="/gameOver" element={<GameOver />} />
         <Route path="/" element={<Home />} />
       </Routes>
+      </Container>
+    
     </BrowserRouter>
   );
 }
---
+++ src/styled-components/Global.js
@@ -0,0 +1,19 @@
+import {createGlobalStyle} from "styled-components";
+
+export default createGlobalStyle`
+* {
+    margin: 0;
+    color: 333;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+
+
+}
+
+h1, h2 {
+    margin-bottom: 2rem;
+}
+
+
+`
